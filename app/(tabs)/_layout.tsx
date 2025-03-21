26d9940e7f169c2f28dc0cc4a57812efce7e0020
wrnkbl <ul0285074@edu.uni.lodz.pl>
2025-03-21 22:02:49 +0100

    _layout update icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,24 +9,24 @@ export default function TabLayout() {
         title: 'Lodówka', 
         headerShown: false,
         tabBarIcon: ({ color, focused }) => (
-          <MaterialCommunityIcons name={focused ? 'fridge' : 'fridge-outline'} color={color} size={24} />
-        ), }} />
+          <MaterialCommunityIcons name={focused ? 'fridge' : 'fridge-outline'} color={focused ? '#72E149' : '#aaa'} size={24} />
+        ), tabBarLabelStyle: { color: '#aaa' }  }} />
 
 
       <Tabs.Screen name="index" options={{ 
         title: 'Strona Główna', 
         headerShown: false,
         tabBarIcon: ({ color, focused }) => (
-          <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
-        ), }}  />
+          <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={focused ? '#72E149' : '#aaa'} size={24} />
+        ), tabBarLabelStyle: { color: '#aaa' } }}  />
 
 
       <Tabs.Screen name="recepies_list" options={{ 
         title: 'Przepisy', 
         headerShown: false,
         tabBarIcon: ({ color, focused }) => (
-          <MaterialCommunityIcons name={focused ? 'clipboard-list' : 'clipboard-list-outline'} color={color} size={24} />
-        ), }} />
+          <MaterialCommunityIcons name={focused ? 'clipboard-list' : 'clipboard-list-outline'} color={focused ? '#72E149' : '#aaa'}  size={24} />
+        ), tabBarLabelStyle: { color: '#aaa' } }} />
     </Tabs>
   );
 }

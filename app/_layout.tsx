ff8189bd209f8e2176c3ac8487a769c67c5192b0
wrnkbl <ul0285074@edu.uni.lodz.pl>
2025-03-22 03:32:11 +0100

    update screen options, status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false, title: "Powrót"  }} />
+    <>
+      <StatusBar style="dark" />
+    <Stack screenOptions={{ headerTintColor: "black", headerBackTitle: "Wstecz" }}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
       <Stack.Screen name="recepie" options={{ headerShown: true, title: "Przepis" }} />
       <Stack.Screen name="new" options={{ headerShown: true, title: "Nowy przepis" }} />
       <Stack.Screen name="lista" options={{ headerShown: true, title: "Lista zakupów" }} />
       <Stack.Screen name="+not-found" />
     </Stack>
+      </>
   );
 }

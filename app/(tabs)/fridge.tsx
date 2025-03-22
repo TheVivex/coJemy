3ee8dea78e88e8f60f4bfad4766bbb3b524d2a50
wrnkbl <ul0285074@edu.uni.lodz.pl>
2025-03-22 03:59:00 +0100

    draft UI fridge

--- app/(tabs)/fridge.tsx
+++ app/(tabs)/fridge.tsx
@@ -1,15 +1,85 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, StyleSheet } from "react-native";
 import { Link } from 'expo-router';
-export default function fridge() {
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+export default function Fridge() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Tu będzie lodówka</Text>
+    <View style={styles.main}>
+      <View style={{ flex: 0.1, flexDirection: "row", justifyContent: "space-between" }}>
+        <Text style={styles.h1}>Lodówka</Text>
+        <Link style={styles.plus} href="/add_fridge">
+          <AntDesign name="pluscircle" size={35} color="#72E149" />
+        </Link>
+      </View>
+
+      <ScrollView style={styles.list}>        
+        <PrintElements />
+      </ScrollView>
     </View>
   );
 }
+
+
+
+const PrintElements = () => {
+  const ingredients = [
+    { name: "Mleko", amount: 1, unit: "L", expirationDate: "2025-04-10" },
+    { name: "Jajka", amount: 10, unit: "szt.", expirationDate: "2025-03-30" },
+    { name: "Ser", amount: 200, unit: "g", expirationDate: "2025-04-15" },
+  ];
+
+  return (
+    <ScrollView>
+      {ingredients.map((item, index) => (
+        <View key={index} style={styles.obj}>
+          <View>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.amount}>{item.amount} {item.unit}</Text>
+          </View>
+          <View style={{display:"flex", alignItems: "center", justifyContent: "center"}}>
+            <Text style={styles.expiration}>Ważne do: {item.expirationDate}</Text>
+          </View>
+          
+        </View>
+      ))}
+    </ScrollView>
+  );
+};
+
+
+
+const styles = StyleSheet.create({
+  main: {
+    paddingTop: 50,
+    paddingLeft: 20,
+    paddingRight: 20,
+    flex: 1,
+  },
+  list: {
+    flex: 1,
+  },
+  h1: {
+    fontSize: 40,
+  },
+  plus: {
+    marginTop: 10,
+  },
+  obj: {
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 1, 
+    fontWeight: "bold",
+    marginBottom: 10,
+    padding: 9,
+    borderRadius: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  expiration: {
+    fontSize: 16,
+  }
+});
